fdf99e282f14c6b35f5bad249949e2db075100c8
sammwaura <meshsami27@gmail.com>
2022-09-21 12:31:48 +0300

    modify

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,5 @@ menu.onclick = () =>{
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
-    
-    if(window.scrollY > 0){
-        header.classList.add('active');       
-    }else{
-        header.classList.remove('active');
-    }
 
 }
